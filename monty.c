bb3e86aa6d228c39c8f7a508eb449df8aa0a5319
Ash C <no@body.example>
2018-07-13 15:41:52 +0000

    Adding global scoreboard for future multithreading support, might go with returning the results from the thread later, will need the type regardless though.

--- monty.c
+++ monty.c
@@ -5,6 +5,18 @@
 #define CLEARSCR "\033[2J"
 #define ZEROCURSOR "\033[H"
 
+typedef struct GameScore {
+  int numWonWSwitch;
+  int numWonWoSwitch;
+  int numLostWSwitch;
+  int numLostWoSwitch;
+
+  float percentWonWSwitch;
+  float percentWonWoSwitch;
+} GameScore;
+
+GameScore *gameScoreTable[64] = {0}; 
+
 const char *ContestantChoiceStr[] = {"to stick with door", "to change to door"};
 int killtime=0;
 
@@ -13,13 +25,14 @@ void sighandler(int num) {
 }
 
 int main() {
-  int numWonWSwitch=0;
-  int numWonWoSwitch=0;
-  int numLostWSwitch=0;
-  int numLostWoSwitch=0;
+  gameScoreTable[0] = calloc(1, sizeof(GameScore));
+//  int numWonWSwitch=0;
+//  int numWonWoSwitch=0;
+//  int numLostWSwitch=0;
+//  int numLostWoSwitch=0;
 
-  float percentWonWSwitch=0.0f;
-  float percentWonWoSwitch=0.0f;
+//  float percentWonWSwitch=0.0f;
+//  float percentWonWoSwitch=0.0f;
 
   long int correctDoor;
   long int chosenDoor;
@@ -36,10 +49,10 @@ int main() {
     printf(ZEROCURSOR);
     decision = random()%2;
     correctDoor = random() % 3;
-    percentWonWSwitch = ((float)numWonWSwitch / ((float)numWonWSwitch+numLostWSwitch)) * 100.0f;
-    percentWonWoSwitch = ((float)numWonWoSwitch / ((float)numWonWoSwitch+numLostWoSwitch)) * 100.0f;
-    printf("Score: (%d:%d Wins to loss \\w switch, %d:%d wins to loss \\wo switch)\n", numWonWSwitch, numLostWSwitch, numWonWoSwitch, numLostWoSwitch);
-    printf("Winning percentage: %.2f%% \\w switch, %.2f%% \\wo switch\n", percentWonWSwitch, percentWonWoSwitch); 
+    gameScoreTable[0]->percentWonWSwitch = ((float)gameScoreTable[0]->numWonWSwitch / ((float)gameScoreTable[0]->numWonWSwitch+gameScoreTable[0]->numLostWSwitch)) * 100.0f;
+    gameScoreTable[0]->percentWonWoSwitch = ((float)gameScoreTable[0]->numWonWoSwitch / ((float)gameScoreTable[0]->numWonWoSwitch+gameScoreTable[0]->numLostWoSwitch)) * 100.0f;
+    printf("Score: (%d:%d Wins to loss \\w switch, %d:%d wins to loss \\wo switch)\n", gameScoreTable[0]->numWonWSwitch, gameScoreTable[0]->numLostWSwitch, gameScoreTable[0]->numWonWoSwitch, gameScoreTable[0]->numLostWoSwitch);
+    printf("Winning percentage: %.2f%% \\w switch, %.2f%% \\wo switch\n", gameScoreTable[0]->percentWonWSwitch, gameScoreTable[0]->percentWonWoSwitch); 
     printf(" - Game %d started, (winning door is %d)\n", num, correctDoor);
     chosenDoor = random() % 3;
     printf(" - Contestant chose door %d\n", chosenDoor);
@@ -56,19 +69,19 @@ int main() {
     printf(" - Contestant decided %s %d        \n", ContestantChoiceStr[decision], (chosenDoor = (decision) ? altDoor:chosenDoor));
 
     if (correctDoor == chosenDoor) {
-      if (decision) numWonWSwitch++;
-      else numWonWoSwitch++;
+      if (decision) gameScoreTable[0]->numWonWSwitch++;
+      else gameScoreTable[0]->numWonWoSwitch++;
       printf(" - Contestant Won \n");
     }
     else {
-      if (decision) numLostWSwitch++;
-      else numLostWoSwitch++;
+      if (decision) gameScoreTable[0]->numLostWSwitch++;
+      else gameScoreTable[0]->numLostWoSwitch++;
       printf(" - Contestant Lost\n");
     }
 
 //getchar(); // Need to make this a command line argument, amount various other things i need to do
   }
-
+  free(gameScoreTable[0]);
   printf("Ending game loop.\n");
 
 };
