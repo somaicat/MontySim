6982a0d5ee61ad53814badc06d9938ea5b6e129d
Ash C <no@body.example>
2018-07-18 13:09:54 +0000

    Early multithreading support implemented

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 This is a small project for simulating Monty Hall style games and measuring the probability of various outcomes.
 Normally I wouldn't bother putting something like this on something like github, but I'm currently having computer storage problems, so this will have to do. Also I've never used github before so this is a good chance to learn more about it.
 
-
+Todo:
+Needs a fair bit of optimization, there's probably variables I don't need.
+Something weird is up with multithreading, for some reason I seem to be losing overall effeciency by using multiple cores. Gonna check what it looks like as far as the threads affinity goes. Needs research.
--- monty.c
+++ monty.c
@@ -5,22 +5,22 @@
 
 #define CLEARSCR "\033[2J"
 #define ZEROCURSOR "\033[H"
-#define CPUNUM 4 // placeholder till I have detection coded
+#define CPUNUM 1 // placeholder till I have detection coded
 const char *ContestantChoiceStr[] = {"to stick with door", "to change to door"};
 int killtime=0;
 int verbose;
 typedef struct GameScore {
-  int numWonWSwitch;
-  int numWonWoSwitch;
-  int numLostWSwitch;
-  int numLostWoSwitch;
+  volatile int numWonWSwitch;
+  volatile int numWonWoSwitch;
+  volatile int numLostWSwitch;
+  volatile int numLostWoSwitch;
 
-  float percentWonWSwitch;
-  float percentWonWoSwitch;
+  volatile float percentWonWSwitch;
+  volatile float percentWonWoSwitch;
 } GameScore;
 
 typedef struct GameThread {
-  pthread_t *thread;
+  pthread_t thread;
   GameScore score;
 } GameThread;
 
@@ -57,9 +57,10 @@ void PlayGame(GameScore *score) {
     // Pick a door to exclude and alternate to offer
     for (excludedDoor=0;excludedDoor==correctDoor || excludedDoor == chosenDoor;excludedDoor++);
     for (altDoor=0;altDoor == chosenDoor || altDoor == excludedDoor;altDoor++);
+    chosenDoor = (decision) ? altDoor:chosenDoor;
     if (verbose) {
       printf(" - Host excluded door %d, option to change to door %d presented\n", excludedDoor, altDoor);
-      printf(" - Contestant decided %s %d        \n", ContestantChoiceStr[decision], (chosenDoor = (decision) ? altDoor:chosenDoor));
+      printf(" - Contestant decided %s %d        \n", ContestantChoiceStr[decision], chosenDoor);
     }
     if (correctDoor == chosenDoor) {
       if (decision) score->numWonWSwitch++;
@@ -71,32 +72,75 @@ void PlayGame(GameScore *score) {
       else score->numLostWoSwitch++;
       if (verbose) printf(" - Contestant Lost\n");
     }
-      if (verbose)
-        getchar(); // Need to make this a command line argument, amount various other things i need to do
+//      if (verbose)
+//        getchar(); // Need to make this a command line argument, amount various other things i need to do
     }
 }
 
 GameThread *StartGame() {
-  GameThread *game = calloc(sizeof(GameThread),1);
-  pthread_create(game->thread, NULL, (void *)(void *) PlayGame,(void*) game);
+  int num;
+  GameThread *game = calloc(1,sizeof(GameThread));
+  pthread_create(&game->thread, NULL, (void *)(void *) PlayGame,(void*) &game->score);
+  return game;
+}
+
+void *MonitorThread(void *arg) {
+  float totalPercentWonWSwitch=0;
+  float totalPercentWonWoSwitch=0;
+  unsigned int totalNumWonWSwitch=0;
+  unsigned int totalNumWonWoSwitch=0;
+  unsigned int totalNumLostWSwitch=0;
+  unsigned int totalNumLostWoSwitch=0;
+  unsigned int totalGames;
+  int num;
+  GameScore *score;
+  printf("Threads started, monitoring progress\n\n");
+  while(!killtime) {
+      printf(ZEROCURSOR);
+      for (num=0; gameThreadTable[num] != NULL; num++) {
+        score = &gameThreadTable[num]->score;
+        printf("[Thread %d] Score: (%d:%d Wins to loss \\w switch, %d:%d wins to loss \\wo switch)\n",num, score->numWonWSwitch, score->numLostWSwitch, score->numWonWoSwitch, score->numLostWoSwitch);
+        printf("[Thread %d] Winning percentage: %.2f%% \\w switch, %.2f%% \\wo switch\n", num, score->percentWonWSwitch, score->percentWonWoSwitch); 
+        totalNumWonWSwitch+=score->numWonWSwitch;
+        totalNumLostWSwitch+=score->numLostWSwitch;
+        totalNumWonWoSwitch+=score->numWonWoSwitch;
+        totalNumLostWoSwitch+=score->numLostWoSwitch;
+      }
+      totalPercentWonWSwitch = ((float)totalNumWonWSwitch / ((float)totalNumWonWSwitch+totalNumLostWSwitch)) * 100.0f;
+      totalPercentWonWoSwitch = ((float)totalNumWonWoSwitch / ((float)totalNumWonWoSwitch+totalNumLostWoSwitch)) * 100.0f;
+      totalGames = totalNumWonWSwitch + totalNumWonWoSwitch + totalNumLostWSwitch + totalNumLostWoSwitch;
+      printf("\nTotal percentages %.2f won switching, %.2f won without switching\n",totalPercentWonWSwitch, totalPercentWonWoSwitch);
+      printf("Total games played: %u\n",totalGames);
+      totalNumWonWSwitch=0;
+      totalNumWonWoSwitch=0;
+      totalNumLostWSwitch=0;
+      totalNumLostWoSwitch=0;
+      usleep(300);
+  }
 }
 
 int main(int argc, int *argv[]) {
   int num;
+  pthread_t monThread;
   gameThreadTable[0] = calloc(1, sizeof(GameThread));
   printf("Installing signal handler...\n");
   signal(SIGINT, sighandler);
 
+  printf(CLEARSCR);
   if (argc > 1 && !strcasecmp(argv[1], "-s"))
     verbose = 1;
   else { // Start multithreaded mode
     verbose=0;
     for (num=0;num<CPUNUM;num++) { 
-
+      gameThreadTable[num] = StartGame();
     }
+    pthread_create(&monThread, NULL, (void *)(void *) MonitorThread, NULL);
+    for (num=0;num<CPUNUM;num++) { 
+      pthread_join(gameThreadTable[num]->thread, 0);
+    }
+    pthread_join(monThread, 0);
   }
 
-  printf(CLEARSCR);
   verbose=1;
   PlayGame(&gameThreadTable[0]->score);
   free(gameThreadTable[0]);
